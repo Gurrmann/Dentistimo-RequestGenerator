c32b637d4e134d77a7729ab32289d90cf08a116c
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 00:58:22 +0100

    Created method to submit requests

--- generator.js
+++ generator.js
@@ -1,14 +1,14 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://test.mosquitto.org')
 
-generateRequest = (random, requestid) => {
+let generateRequest = (random, requestid) => {
 
     var issuance = new Date()
     issuance = issuance.getTime();
 
     let userId = Math.floor(Math.random() * 1000000000)
     requestid += 2000;
-    let dentistid = random ? 4 : Math.floor(Math.random() * 4)
+    let dentistid = random ? Math.floor(Math.random() * 4) : 4
     let time = '1234-12-1 14:30'
     
 
@@ -26,4 +26,12 @@ generateRequest = (random, requestid) => {
   // client.publish('BookingRequest', JSON.stringify(request))
     return request;
 }
-generateRequest()
+
+let submitRequest = (random, amount) => {
+
+    for (i=0; i < amount; i++) {
+       console.log(generateRequest(random, i))
+
+    }
+}
+submitRequest(true,10)
