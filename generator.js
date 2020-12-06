7e0a295835432c14aa79c81d7c5a10b2e3307f83
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 03:29:18 +0100

    Can now send requests in an interval

--- generator.js
+++ generator.js
@@ -1,19 +1,17 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://test.mosquitto.org')
+//var mqtt = require('mqtt');
+//var client = mqtt.connect('mqtt://test.mosquitto.org')
+
+const { setInterval } = require("timers");
 
 let generateRequest = (random, requestid) => {
 
     var issuance = new Date()
     issuance = issuance.getTime();
-
     let userId = Math.floor(Math.random() * 1000000000)
     requestid += 2000;
     let dentistid = random ? Math.floor(Math.random() * 4) : 4
     let time = '1234-12-1 14:30'
     
-
-
-
     let request = {
 
         userid: userId,
@@ -24,14 +22,22 @@ let generateRequest = (random, requestid) => {
 
     }
   // client.publish('BookingRequest', JSON.stringify(request))
-    return request;
+    return request
 }
 
 let submitRequest = (random, amount) => {
 
-    for (i=0; i < amount; i++) {
-       console.log(generateRequest(random, i))
-
-    }
+        let i = 0
+        let interval = setInterval(() => {
+            if(i > amount - 1) {
+              clearInterval(interval)
+         }
+         console.log(generateRequest(random,i))
+         console.log(i)
+          i++
+         },1000);
+        
 }
-submitRequest(true,10)
+submitRequest(false,10)
+
+
