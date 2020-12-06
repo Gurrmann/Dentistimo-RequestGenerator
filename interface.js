de096aa92ce96d276e573c3ba24e5ec29d2cc5b9
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 06:29:11 +0100

    Revamping menu

--- interface.js
+++ interface.js
@@ -1,33 +1,37 @@
-const { generate } = require('mqtt-packet');
-
+const requestGenerator = require('./generator')
 const prompt = require('prompt-sync')({sigint: true});
 const menu = () => {
-  let exit = false;
-
-  while (!exit) {
 
     let message = 'DENTISTIMO REQUEST GENERATOR \n'
-    message += 'Press 1 to send 100 of the same requests to the same dentist \n'
-    message += 'Press 2 to send 100 of the same requets to multiple dentists \n'
-    message += 'Press 0 to exit!'
+    message += 'Press 1 to create your own requests \n'
+    message += 'Press 2 to select from presets \n'
     console.log(message)
-
-    let input = prompt('');
+    let input = prompt()
+    
     switch (input) {
 
       case '1':
-        requestGenerator.submitRequest(false, 100)
+        createRequests()
         break;
       case '2':
-        requestGenerator.submitRequest(true, 100)
+        selectPresets()
         break;
-      case '0':
-        exit = true;
+      default:
+        console.log('Select a valid option \n')
+        menu()
         break;
-        
-      
 
     }
-  }
+
+
 }
+
+var createRequests = () => {
+  console.log('poop')
+}
+
+var selectPresets = () => {
+  console.log('tja')
+}
+
 menu()
