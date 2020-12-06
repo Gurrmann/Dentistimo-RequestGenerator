defbf07b0544db25e1d39867c5e3a0c5197ef93d
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 04:56:30 +0100

    Increments time with 30 minutes if random is selected

--- generator.js
+++ generator.js
@@ -3,25 +3,47 @@
 
 const { setInterval } = require("timers");
 
-let generateRequest = (random, requestid) => {
+let generateRequest = (random, amount) => {
 
-    var issuance = new Date()
-    issuance = issuance.getTime();
+    var date = new Date()
+    issuance = date.getTime();
     let userId = Math.floor(Math.random() * 1000000000)
-    requestid += 2000;
     let dentistid = random ? Math.floor(Math.random() * 4) : 4
-    let time = '1234-12-1 14:30'
+    let time
+    let timeIncrementer = 30
+    if (random) {
+        for (i = 0; i < amount; i++) {
+            time = new Date(date.getTime() + 1000 * 60 * timeIncrementer)
+           // console.log(timeIncrementer)
+           // console.log(i)
+            timeIncrementer += 30
+        }
+    } else {
+        time = '2021-12-1 14:30'
+    }
     
+    amount += 2000;
     let request = {
 
         userid: userId,
-        requestid: requestid,
+        requestid: amount,
         dentistid: dentistid,
         issuance: issuance,
         time: time, 
 
     }
+    
     return request
+}
+
+let dateIncrementer = (amount) => {
+
+    time = new Date()
+
+        while (i < amount) {
+
+        }
+
 }
 
 let submitRequest = (random, amount, time) => {
@@ -29,7 +51,7 @@ let submitRequest = (random, amount, time) => {
         time = time * 1000 //converts seconds in to milliseconds
         let interval = time / amount
 
-        let i = 0
+        let i = 1
         let requests = setInterval(() => {
             if(i > amount - 1) {
               clearInterval(requests)
@@ -40,6 +62,6 @@ let submitRequest = (random, amount, time) => {
          },interval);
         
 }
-submitRequest(true,100,10)
+submitRequest(true,'10','10')
 
 
