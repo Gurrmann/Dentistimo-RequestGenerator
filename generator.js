2dc27150ed21ef2f478ad6de2ffc5d86fcddf348
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 05:19:39 +0100

    Can now send incremented dates in correct format

--- generator.js
+++ generator.js
@@ -1,22 +1,27 @@
 //var mqtt = require('mqtt');
 //var client = mqtt.connect('mqtt://test.mosquitto.org')
-
 const { setInterval } = require("timers");
 
 let generateRequest = (random, amount) => {
 
-    var date = new Date()
-    issuance = date.getTime();
+    var issuance = new Date()
+    var testDate = new Date(2022,0,1,0,0)
+    issuance = issuance.getTime();
     let userId = Math.floor(Math.random() * 1000000000)
     let dentistid = random ? Math.floor(Math.random() * 4) : 4
     let time
+    let format = []
     let timeIncrementer = 30
+    let dateString
     if (random) {
         for (i = 0; i < amount; i++) {
-            time = new Date(date.getTime() + 1000 * 60 * timeIncrementer)
-           // console.log(timeIncrementer)
-           // console.log(i)
+            time = new Date(testDate.getTime() + 1000 * 60 * timeIncrementer)
             timeIncrementer += 30
+            dateString = time.toISOString()
+            format[0] = dateString.slice(0,10)
+            format[1] = dateString.slice(11,16)
+            dateString = format[0] + ' ' + format[1]
+            //console.log(dateString)
         }
     } else {
         time = '2021-12-1 14:30'
@@ -29,7 +34,7 @@ let generateRequest = (random, amount) => {
         requestid: amount,
         dentistid: dentistid,
         issuance: issuance,
-        time: time, 
+        time: dateString, 
 
     }
     
@@ -56,12 +61,12 @@ let submitRequest = (random, amount, time) => {
             if(i > amount - 1) {
               clearInterval(requests)
          }
-         console.log(generateRequest(random,i))
-         console.log(i)
+        console.log(generateRequest(random,i))
+        // console.log(i)
           i++
          },interval);
         
 }
-submitRequest(true,'10','10')
+submitRequest(true,'100','10')
 
 
