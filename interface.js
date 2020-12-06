cce4c7901b1343ecb3bd7e5bbd91524f18181fbb
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-06 01:03:52 +0100

    Added 2 scenarios to the menAdded 2 scenarios to the menuu

--- interface.js
+++ interface.js
@@ -1,3 +1,5 @@
+const { generate } = require('mqtt-packet');
+
 const prompt = require('prompt-sync')({sigint: true});
 const menu = () => {
   let exit = false;
@@ -5,12 +7,20 @@ const menu = () => {
   while (!exit) {
 
     let message = 'DENTISTIMO REQUEST GENERATOR \n'
+    message += 'Press 1 to send 100 of the same requests to the same dentist \n'
+    message += 'Press 2 to send 100 of the same requets to multiple dentists \n'
     message += 'Press 0 to exit!'
     console.log(message)
 
     let input = prompt('');
     switch (input) {
 
+      case '1':
+        requestGenerator.submitRequest(false, 100)
+        break;
+      case '2':
+        requestGenerator.submitRequest(true, 100)
+        break;
       case '0':
         exit = true;
         break;
