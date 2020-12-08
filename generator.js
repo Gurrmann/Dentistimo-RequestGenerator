faf4023a977c8e40dd2772df1336070e734e91fd
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-08 23:53:26 +0100

    Number of dentists now in requests

--- generator.js
+++ generator.js
@@ -4,7 +4,7 @@ const { setInterval } = require("timers");
 
 let generateRequest = (requestData, requestid) => {
 
-    var issuance = new Date()
+    let issuance = new Date()
     issuance = issuance.getTime();
     let userId = Math.floor(Math.random() * 1000000000)
     let dentistid = requestData.randomDentistry ? Math.floor(Math.random() * 4) : 4
@@ -31,16 +31,39 @@ let generateRequest = (requestData, requestid) => {
         dateString = '2021-12-1 14:30'
 
     }
+    // assigns a fixed amount of dentists to the dentistry
+    let dentistNumber
+    switch (dentistid) {
     
+        case 1:
+            dentistNumber = 2
+            break;
+
+        case 2:
+            dentistNumber = 1
+            break
+        
+        case 3:
+            dentistNumber = 4
+            break
+
+        case 4:
+            dentistNumber = 2
+            break
+        
+
+    }
+
     //arbitrary number to not conflict with real requests,
     requestid += 1000000;
     let request = {
-
+    
         userid: userId,
         requestid: requestid,
         dentistid: dentistid,
         issuance: issuance,
         time: dateString, 
+        numberOfDentists: dentistNumber
 
     }
     
